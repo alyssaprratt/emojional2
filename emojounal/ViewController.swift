7c43e67cfba4d6692f7db5ff47eaa2097bf13e97
alyssa Pratt <magoopratt@gmail.com>
2019-06-10 07:52:52 -0700

    initial commit

---
+++ emojounal/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  emojounal
+//
+//  Created by Apple on 6/7/19.
+//  Copyright © 2019 Apple. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func happy(_ sender: Any) {
+        
+    }
+    
+    
+}
+
